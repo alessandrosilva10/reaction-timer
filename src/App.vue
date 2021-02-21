d2a6d0cb4613aaeaf3215e1e3b81d7aaec902b30
Alessandro <alessandro.mpgh@gmail.com>
2021-02-21 14:43:09 -0300

    Adding new components and its functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-
+  <div>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play!</button>
+    <Block v-if="isPlaying" :delay="delay"/>
+  </div>
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Block
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000 //ms
+      this.isPlaying = true;
+    }
   }
 }
 </script>
@@ -19,7 +35,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
